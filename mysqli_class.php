bdcdb42e3d15233a5ba345cb671d55ec85190e1d
Yang En Qing <zbzcyeq@163.com>
2014-09-04 16:47:36 +0800

    Update mysqli_class.php

--- mysqli_class.php
+++ mysqli_class.php
@@ -156,9 +156,10 @@ class mysqli_class {
    
         if (version_compare(PHP_VERSION, '5.3.0') >= 0) { 
             $refs = array(); 
-            foreach($arr as $key => $value) 
+            foreach($arr as $key => $value) {
                 $refs[$key] = &$arr[$key]; 
                 return $refs; 
+            }
         }
         return $arr;
    
